a624b5e32cbfe24d863b639a7edc56cffe699fd4
Madderstone <anthony.issa@hotmail.fr>
2023-02-23 17:44:18 +0100

    Background script working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { getElementByXpath } from "../scripts/utils/nodeUtils";
+import { saveChatNodeButton } from "./content-scripts/saveChat";
 
-console.log(getElementByXpath('//*[@id="__next"]/div[1]/div[2]/div/div/nav/a[1]'))
+
+
+const form = getElementByXpath('//*[@id="__next"]/div[1]/div[1]/main/div[2]/form');
+form.appendChild(saveChatNodeButton())
+
+console.log("test")

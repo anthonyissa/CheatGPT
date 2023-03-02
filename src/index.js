b2092c062e46721b8bc6e660c7b56f84fd5c54aa
Madderstone <anthony.issa@hotmail.fr>
2023-03-02 18:17:21 +0100

    v1

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { getElementByXpath } from "../scripts/utils/nodeUtils";
-import { saveChatNodeButton } from "./content-scripts/saveChat";
-
-
-
-const form = getElementByXpath('//*[@id="__next"]/div[1]/div[1]/main/div[2]/form');
-form.appendChild(saveChatNodeButton())
-
-console.log("test")
